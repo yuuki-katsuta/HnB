22ce5070ae7b783ea60a86cadb22127c93d245f2
yuuki-katsuta <reptpr0314@gmail.com>
2022-03-06 20:52:31 +0900

    バグ修正

--- src/logic/nameValidate.ts
+++ src/logic/nameValidate.ts
@@ -1,5 +1,5 @@
 export const nameValidate = (name: string) => {
-  const isLengthValid = name.length > 10;
-  const isEmpty = name.trim() !== '';
+  const isLengthValid = name.length <= 10;
+  const isEmpty = name.trim() === '';
   return isLengthValid && isEmpty;
 };
