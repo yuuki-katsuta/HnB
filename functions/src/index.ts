80aa648569818b34bdce6468d0ee6899b33d020c
tpryuki <reptpr0314@gmail.com>
2025-07-28 03:39:54 +0900

    refactor: clean up dependencies and format code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,40 +1,40 @@
-import functions = require('firebase-functions');
-import admin = require('firebase-admin');
+import functions = require("firebase-functions");
+import admin = require("firebase-admin");
 
 admin.initializeApp();
 
 const deleteDocumentRecursively = async (
-  docRef: admin.firestore.DocumentReference<admin.firestore.DocumentData>
+    docRef: admin.firestore.DocumentReference<admin.firestore.DocumentData>
 ) => {
   const collections = await docRef.listCollections();
   if (collections.length > 0) {
     for (const collection of collections) {
       const snapshot = await collection.get();
       for (const doc of snapshot.docs) {
-        //単独のドキュメントに対して実行
+        // 単独のドキュメントに対して実行
         await deleteDocumentRecursively(doc.ref);
       }
     }
   }
-  await docRef.delete(); //サブコレクションを削除してから実行
+  await docRef.delete(); // サブコレクションを削除してから実行
 };
 
 exports.clearRoomData = functions
-  .region('asia-northeast1')
-  .runWith({
-    timeoutSeconds: 300,
-    memory: '2GB',
-  })
-  .pubsub.schedule('0 0 * * *') //毎日0:00に実行
-  .timeZone('Asia/Tokyo')
-  .onRun(async () => {
-    const db = admin.firestore();
-    const ref = db.collection('rooms');
-    const snapshot = await ref.get();
+    .region("asia-northeast1")
+    .runWith({
+      timeoutSeconds: 300,
+      memory: "2GB",
+    })
+    .pubsub.schedule("0 0 * * *") // 毎日0:00に実行
+    .timeZone("Asia/Tokyo")
+    .onRun(async () => {
+      const db = admin.firestore();
+      const ref = db.collection("rooms");
+      const snapshot = await ref.get();
 
-    await Promise.all(
-      snapshot.docs.map(async (doc) => {
-        await deleteDocumentRecursively(db.collection('rooms').doc(doc.id));
-      })
-    ).catch((error) => error.message);
-  });
+      await Promise.all(
+          snapshot.docs.map(async (doc) => {
+            await deleteDocumentRecursively(db.collection("rooms").doc(doc.id));
+          })
+      ).catch((error) => error.message);
+    });
